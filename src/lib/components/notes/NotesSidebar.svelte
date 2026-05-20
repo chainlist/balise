d8c00a8cce5931bd9da67ebd3d52bcaa854e2f55
kguillouard <kguillouard@equativ.com>
2026-05-20 03:05:53 +0200

    fix: notes sidebar white bg, distinct from nav sidebar

--- src/lib/components/notes/NotesSidebar.svelte
+++ src/lib/components/notes/NotesSidebar.svelte
@@ -17,7 +17,7 @@
 	} = $props();
 </script>
 
-<Sidebar.Root collapsible="none" class="border-r border-sidebar-border">
+<Sidebar.Root collapsible="none" class="bg-card text-foreground border-r border-sidebar-border">
 	<NotesSidebarHeader {onCreate} />
 	<NotesSidebarContent {notes} {selectedNoteId} {onSelect} />
 </Sidebar.Root>
